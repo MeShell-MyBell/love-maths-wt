b77886c19e52650019eb23bcd5edd8be533a0926
Meshell-Mybell <shellie_w@live.com.au>
2025-06-18 16:59:33 +0100

    displaying addition question

--- js/script.js
+++ js/script.js
@@ -9,6 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+
+                if (gameType === "addition") {
+                    displayAdditionQuestion(num1, num2);
+                }
+               
             }
         });
     }
@@ -41,8 +46,11 @@ function incrementWrongAnswer () {
 
 }
 
-function displayAdditionQuestion () {
-
+function displayAdditionQuestion (operand 1, operand2) {
+   
+    document.getElementById('operand1'").textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 function displaySubtractQuestion () {
 
